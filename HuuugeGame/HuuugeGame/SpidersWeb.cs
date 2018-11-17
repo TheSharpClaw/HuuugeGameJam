fa34ea68681ea97a90c028cc7a237262e58413f9
Wiktor <Wiktor@DESKTOP-TJHUHFC>
2018-11-17 18:52:19 +0100

    spider drawing and spidersweb drawing fix

---
+++ HuuugeGame/HuuugeGame/SpidersWeb.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuuugeGame
+{
+    class SpidersWeb
+    {
+        private Vector2 position;
+        private Vector2 size;
+        private int power;
+        public SpidersWeb(Vector2 position, Vector2 size, int power)
+        {
+            this.Position = position;
+            this.Size = size;
+            this.Power = power;
+        }
+
+        public Vector2 Position { get => position; set => position = value; }
+        public Vector2 Size { get => size; set => size = value; }
+        public int Power { get => power; set => power = value; }
+    }
+}
