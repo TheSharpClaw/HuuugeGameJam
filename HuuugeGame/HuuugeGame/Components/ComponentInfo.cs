4b143e970e39a70eb6f591a35cd6de10185ef044
Wiktor <Wiktor@DESKTOP-TJHUHFC>
2018-11-18 10:15:12 +0100

    przyciemnienie ekranu info i gamewin

--- HuuugeGame/HuuugeGame/Components/ComponentInfo.cs
+++ HuuugeGame/HuuugeGame/Components/ComponentInfo.cs
@@ -27,13 +27,13 @@ namespace HuuugeGame.Components
         {
             Globals.spriteBatch.Begin();
             Globals.spriteBatch.Draw(Globals.backgroundTexture, new Vector2(0, 0), Color.White);
-            Globals.spriteBatch.Draw(Globals.hpBar, new Rectangle((int)Globals.screenSize.X/2-Globals.hpBar.Width, 47, 132, 22), new Color(0, 0, 0, 150));
+            Globals.spriteBatch.Draw(Globals.hpBar, new Rectangle(0, 0, 700, 600), new Color(0, 0, 0, 150));
+
             Globals.spriteBatch.DrawString(Globals.defaultFont, "STEROWANIE", new Vector2(Globals.screenSize.X/2-50, 50), Color.White);
             Globals.spriteBatch.Draw(Globals.spiderStaticTexture, new Rectangle((int)Globals.screenSize.X / 4 - Globals.spiderStaticTexture.Width, 100, Globals.spiderStaticTexture.Width * 2, Globals.spiderStaticTexture.Height * 2), Color.White);
             Globals.spriteBatch.Draw(Globals.motherFlyStaticTexture, new Rectangle(((int)Globals.screenSize.X / 4) * 3 - Globals.motherFlyStaticTexture.Width, 100, Globals.motherFlyStaticTexture.Width * 2, Globals.motherFlyStaticTexture.Height * 2), Color.White);
             Globals.spriteBatch.Draw(Globals.wsadIMG, new Rectangle(((int)Globals.screenSize.X / 4) - Globals.wsadIMG.Width/2, (100 + Globals.spiderStaticTexture.Height * 2+40), Globals.wsadIMG.Width, Globals.wsadIMG.Height),Color.White);
             Globals.spriteBatch.Draw(Globals.shiftIMG, new Rectangle(((int)Globals.screenSize.X / 4) - Globals.shiftIMG.Width / 2, (100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height +103), Globals.shiftIMG.Width, Globals.shiftIMG.Height),Color.White);
-            //Globals.spriteBatch.Draw(Globals.hpBar, new Rectangle((int)Globals.screenSize.X / 4 - Globals.hpBar.Width, (100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 60), 132, 22), new Color(0, 0, 0, 150));
             Globals.spriteBatch.DrawString(Globals.defaultFont, "press to place a web", new Vector2(Globals.screenSize.X / 4 - 75, 100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 70), Color.White);
             Globals.spriteBatch.DrawString(Globals.defaultFont, "Target: Eat every small butterflies", new Vector2(Globals.screenSize.X / 4 - 115, 100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 190), Color.White);
             Globals.spriteBatch.DrawString(Globals.defaultFont, "Target: Collect as many\nbutterflies as possible", new Vector2((Globals.screenSize.X / 4)*3 - 75, 100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 70), Color.White);
