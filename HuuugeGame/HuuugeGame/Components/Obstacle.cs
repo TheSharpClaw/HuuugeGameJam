00b6d8468afdcbdc9e71eb52e1648a032145e76a
Wiktor <Wiktor@DESKTOP-TJHUHFC>
2018-11-18 08:20:53 +0100

    elo elo 3 2 0

---
+++ HuuugeGame/HuuugeGame/Components/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using HuuugeGame.Behaviour;
+using HuuugeGame.Content.Behaviour;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HuuugeGame
+{
+    public class Obstacle : IEntity
+    {
+        public Vector2 Position { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public Texture2D Texture { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public Rectangle BoundingBox { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public IComponent stage => throw new System.NotImplementedException();
+
+        public void Draw()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
