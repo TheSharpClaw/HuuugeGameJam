6982bedbcc680ff926faf19cfd5027e2047aa99c
Kamil Z <zawadzki.kamil2@gmail.com>
2018-11-18 07:40:30 +0100

    INFO STATE LIKE A BOSS

--- HuuugeGame/HuuugeGame/Components/ComponentInfo.cs
+++ HuuugeGame/HuuugeGame/Components/ComponentInfo.cs
@@ -17,7 +17,7 @@ namespace HuuugeGame.Components
         {
             goBack = new Button(Globals.yellowButton, Globals.defaultFont, "BACK")
             {
-                Position = new Vector2((Globals.screenSize.X - Globals.yellowButton.Width)/2, Globals.screenSize.Y - 100),
+                Position = new Vector2((3*(Globals.screenSize.X/4) - Globals.yellowButton.Width/2), Globals.screenSize.Y - 100),
                 Text = "Quit"
             };
             goBack._isSelected = true;
@@ -31,8 +31,13 @@ namespace HuuugeGame.Components
             Globals.spriteBatch.DrawString(Globals.defaultFont, "STEROWANIE", new Vector2(Globals.screenSize.X/2-50, 50), Color.White);
             Globals.spriteBatch.Draw(Globals.spiderStaticTexture, new Rectangle((int)Globals.screenSize.X / 4 - Globals.spiderStaticTexture.Width, 100, Globals.spiderStaticTexture.Width * 2, Globals.spiderStaticTexture.Height * 2), Color.White);
             Globals.spriteBatch.Draw(Globals.motherFlyStaticTexture, new Rectangle(((int)Globals.screenSize.X / 4) * 3 - Globals.motherFlyStaticTexture.Width, 100, Globals.motherFlyStaticTexture.Width * 2, Globals.motherFlyStaticTexture.Height * 2), Color.White);
-            Globals.spriteBatch.Draw(Globals.wsadIMG, new Rectangle(((int)Globals.screenSize.X / 4) - Globals.wsadIMG.Width/4, (100 + Globals.spiderStaticTexture.Height * 2+55), Globals.wsadIMG.Width/2, Globals.wsadIMG.Height/2),Color.White);
-            Globals.spriteBatch.Draw(Globals.wsadIMG, new Rectangle(((int)Globals.screenSize.X / 4)*3 - Globals.wsadIMG.Width/4, (100 + Globals.motherFlyStaticTexture.Height * 2+55), Globals.wsadIMG.Width/2, Globals.wsadIMG.Height/2),Color.White);
+            Globals.spriteBatch.Draw(Globals.wsadIMG, new Rectangle(((int)Globals.screenSize.X / 4) - Globals.wsadIMG.Width/2, (100 + Globals.spiderStaticTexture.Height * 2+40), Globals.wsadIMG.Width, Globals.wsadIMG.Height),Color.White);
+            Globals.spriteBatch.Draw(Globals.shiftIMG, new Rectangle(((int)Globals.screenSize.X / 4) - Globals.shiftIMG.Width / 2, (100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height +103), Globals.shiftIMG.Width, Globals.shiftIMG.Height),Color.White);
+            //Globals.spriteBatch.Draw(Globals.hpBar, new Rectangle((int)Globals.screenSize.X / 4 - Globals.hpBar.Width, (100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 60), 132, 22), new Color(0, 0, 0, 150));
+            Globals.spriteBatch.DrawString(Globals.defaultFont, "press to place a web", new Vector2(Globals.screenSize.X / 4 - 75, 100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 70), Color.White);
+            Globals.spriteBatch.DrawString(Globals.defaultFont, "Target: Eat every small butterflies", new Vector2(Globals.screenSize.X / 4 - 115, 100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 190), Color.White);
+            Globals.spriteBatch.DrawString(Globals.defaultFont, "Target: Collect as many\nbutterflies as possible", new Vector2((Globals.screenSize.X / 4)*3 - 75, 100 + Globals.spiderStaticTexture.Height * 2 + Globals.wsadIMG.Height + 70), Color.White);
+            Globals.spriteBatch.Draw(Globals.arrowsIMG, new Rectangle(((int)Globals.screenSize.X / 4)*3 - Globals.arrowsIMG.Width/2, (100 + Globals.motherFlyStaticTexture.Height * 2+40), Globals.arrowsIMG.Width, Globals.arrowsIMG.Height),Color.White);
             goBack.Draw(Globals.spriteBatch);
             Globals.spriteBatch.End();
         }
